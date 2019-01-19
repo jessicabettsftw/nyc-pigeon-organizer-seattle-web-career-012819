dc6ae4eeea3ba81245af0d5cd193825c1e6a7056
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-19 02:57:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,28 @@
 def nyc_pigeon_organizer(data)
   new_pigeon = {}
-  pigeon_names = []
+  pigeon_names = get_names(data)
+  
+  
 
   data.each do |descripter, values|
     values.each do |adjective, values2|
       values2.each do |names|
         puts "new_pigeon[#{names}][#{descripter}] = #{adjective}"
-        new_pigeon[names]
+        new_pigeon[names] = {}
+        new_pigeon[names][]
       end
     end
   end
   new_pigeon
+end
+
+def get_names(data)
+  names =[]
+  
+  colors = data[:color]
+  colors.each do |color, array|
+    array.each do |name|
+      names.push(name)
+    end
+  end
 end
