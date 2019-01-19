29f2c353e8d490f7b7d736933c5e7c036fa0e160
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-19 03:31:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,6 +13,7 @@ def nyc_pigeon_organizer(data)
           if ((new_pigeon[names].keys).include? descripter) == false
             #puts "new descripter"
             new_pigeon[names][descripter] = []
+            new_pigeon[names][descripter] << adjective.to_s
           else
             new_pigeon[names][descripter] << adjective.to_s
           end
