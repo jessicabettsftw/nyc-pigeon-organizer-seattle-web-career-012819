c2ef6d40d94c580943a5549b23452846ef9bdb16
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-19 03:32:02 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon = {}
+
+  data.each do |descripter, values|
+    values.each do |adjective, values2|
+      values2.each do |names|
+        if ((new_pigeon.keys).include? names) == false
+          new_pigeon[names] = {}
+          new_pigeon[names][descripter] = []
+          new_pigeon[names][descripter] << adjective.to_s
+        else
+          if ((new_pigeon[names].keys).include? descripter) == false
+            #puts "new descripter"
+            new_pigeon[names][descripter] = []
+            new_pigeon[names][descripter] << adjective.to_s
+          else
+            new_pigeon[names][descripter] << adjective.to_s
+          end
+        end
+      end
+    end
+  end
+  new_pigeon
 end
