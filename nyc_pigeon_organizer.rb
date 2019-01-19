b0e864be06c57b4655f094ae352c76cb27d12b37
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-19 02:34:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,8 +5,10 @@ def nyc_pigeon_organizer(data)
   data.each do |descripter, values|
     values.each do |adjective, values2|
       values2.each do |names|
-        puts"#{names}"
-    
+        puts"new_pigeon[#{names}][#{descripter}] = [#{adjective}]"
+      end
+    end
+  end
   
   new_pigeon
 end
